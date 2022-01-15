06e57742d4556eff6065a95aee30b287c8f2e29a
Ahmed Hassan <ahmed__hassan@outlook.com>
2022-01-15 20:33:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,9 +13,25 @@ import App from './components/App';
 import { API } from "@aws-amplify/api";
 import { Auth } from "@aws-amplify/auth";
 import awsconfig from './aws-exports';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import ModalManager from "./components/ModalManager";
 import WindowSizeListener from "./components/WindowSizeListener";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5qtc5aQFVBclDwYalib-qXn7DdK-tLEk",
+  authDomain: "forwardslash.firebaseapp.com",
+  projectId: "forwardslash",
+  storageBucket: "forwardslash.appspot.com",
+  messagingSenderId: "637088123316",
+  appId: "1:637088123316:web:60a68d05274be716828ae1",
+  measurementId: "G-XJM3GQR6VP"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 API.configure(awsconfig);
 Auth.configure(awsconfig);
