4ef878800dae249cf486acee320b18f10cb27764
Ahmed Hassan <ahmed__hassan@outlook.com>
2022-02-24 18:30:50 +0200

    Doing Some Tests

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
