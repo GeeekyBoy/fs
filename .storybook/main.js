dbd77cd8cc7a401567fb452db1c013211ccfa85e
Ahmed Hassan <ahmed__hassan@outlook.com>
2022-03-01 15:02:28 +0200

    Removing Redundant Code

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
@@ -14,32 +12,5 @@ module.exports = {
     "@storybook/addon-a11y",
     "storybook-addon-breakpoints",
     "storybook-addon-performance/register"
-  ],
-  webpackFinal: async (config, { configType }) => {
-    config.module.rules.map((rule) => {
-      if (rule.oneOf) {
-        rule.oneOf = rule.oneOf.slice().map((subRule) => {
-          if (subRule.test instanceof RegExp && (subRule.test.test(".module.scss") || subRule.test.test(".scss"))) {
-            return {
-              ...subRule,
-              use: [
-                ...subRule.use,
-                {
-                  loader: require.resolve("sass-resources-loader"),
-                  options: {
-                    resources: [
-                      path.resolve(__dirname, "../src/scss/shared.scss"),
-                    ],
-                  },
-                },
-              ],
-            };
-          }
-          return subRule;
-        });
-      }
-      return rule;
-    });
-    return config;
-  },
+  ]
 };
