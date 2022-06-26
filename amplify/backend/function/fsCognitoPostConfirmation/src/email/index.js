0fec14785e898050fb16a9d9739761d2c605965a
Ahmed Hassan <ahmed__hassan@outlook.com>
2022-06-26 19:58:53 +0200

    Modifying amplify.yml

--- amplify/backend/function/fsCognitoPostConfirmation/src/email/index.js
+++ amplify/backend/function/fsCognitoPostConfirmation/src/email/index.js
@@ -2,26 +2,26 @@ const fs = require('fs')
 const path = require('path')
 
 const getTemplate = (templateID) => {
-    const templateRaw = fs.readFileSync(path.resolve(__dirname, `${templateID}.html`), 'utf8')
-    return {
-        subject: templateRaw.substring(0, templateRaw.indexOf('\n')),
-        body: templateRaw.substring(templateRaw.indexOf('\n') + 1)
-    }
+  const templateRaw = fs.readFileSync(path.resolve(__dirname, `${templateID}.html`), 'utf8')
+  return {
+    subject: templateRaw.substring(0, templateRaw.indexOf('\n')),
+    body: templateRaw.substring(templateRaw.indexOf('\n') + 1)
+  }
 }
 
 const templates = {
-    accountCreation: getTemplate("accountCreation")
+  accountCreation: getTemplate("accountCreation")
 }
 
 const getContent = (templateID, entities) => {
-    const result = {...templates[templateID]}
-    const entitiesKeyValuePairs = Object.entries(entities)
-    for (const entitiesKeyValuePair of entitiesKeyValuePairs) {
-        const replacementRegex = new RegExp(`\\[${entitiesKeyValuePair[0]}\\]`, "g")
-        result.subject = result.subject.replace(replacementRegex, entitiesKeyValuePair[1])
-        result.body = result.body.replace(replacementRegex, entitiesKeyValuePair[1])
-    }
-    return result
-}
+  const result = { ...templates[templateID] };
+  const entitiesKeyValuePairs = Object.entries(entities);
+  entitiesKeyValuePairs.forEach(([key, value]) => {
+    const replacementRegex = new RegExp(`\\[${key}\\]`, 'g');
+    result.subject = result.subject.replace(replacementRegex, value);
+    result.body = result.body.replace(replacementRegex, value);
+  });
+  return result;
+};
 
-module.exports = { getContent }
+module.exports = { getContent };
--- amplify/backend/function/fsCoreApiResolver/src/email/index.js
+++ amplify/backend/function/fsCoreApiResolver/src/email/index.js
@@ -1,28 +1,28 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 const getTemplate = (templateID) => {
-    const templateRaw = fs.readFileSync(path.resolve(__dirname, `${templateID}.html`), 'utf8')
-    return {
-        subject: templateRaw.substring(0, templateRaw.indexOf('\n')),
-        body: templateRaw.substring(templateRaw.indexOf('\n') + 1)
-    }
-}
+  const templateRaw = fs.readFileSync(path.resolve(__dirname, `${templateID}.html`), 'utf8');
+  return {
+    subject: templateRaw.substring(0, templateRaw.indexOf('\n')),
+    body: templateRaw.substring(templateRaw.indexOf('\n') + 1),
+  };
+};
 
 const templates = {
-    assignment: getTemplate("assignment"),
-    assignmentWatching: getTemplate("assignmentWatching")
-}
+  assignment: getTemplate('assignment'),
+  assignmentWatching: getTemplate('assignmentWatching'),
+};
 
 const getContent = (templateID, entities) => {
-    const result = {...templates[templateID]}
-    const entitiesKeyValuePairs = Object.entries(entities)
-    for (const entitiesKeyValuePair of entitiesKeyValuePairs) {
-        const replacementRegex = new RegExp(`\\[${entitiesKeyValuePair[0]}\\]`, "g")
-        result.subject = result.subject.replace(replacementRegex, entitiesKeyValuePair[1])
-        result.body = result.body.replace(replacementRegex, entitiesKeyValuePair[1])
-    }
-    return result
-}
+  const result = { ...templates[templateID] };
+  const entitiesKeyValuePairs = Object.entries(entities);
+  entitiesKeyValuePairs.forEach(([key, value]) => {
+    const replacementRegex = new RegExp(`\\[${key}\\]`, 'g');
+    result.subject = result.subject.replace(replacementRegex, value);
+    result.body = result.body.replace(replacementRegex, value);
+  });
+  return result;
+};
 
-module.exports = { getContent }
+module.exports = { getContent };
