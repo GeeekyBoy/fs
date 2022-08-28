3a7ff8e55cc4977a7ebe84f30d01592a6c0331fe
Ahmed Hassan <ahmed__hassan@outlook.com>
2022-08-28 12:58:14 +0200

    bug fix

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,51 +1,45 @@
 export type AmplifyDependentResourcesAttributes = {
-  api: {
-    fsCoreAPI: {
-      GraphQLAPIIdOutput: "string";
-      GraphQLAPIEndpointOutput: "string";
-    };
-  };
-  auth: {
-    userPoolGroups: {
-      freeGroupRole: "string";
-      proGroupRole: "string";
-    };
-    fsCognito: {
-      IdentityPoolId: "string";
-      IdentityPoolName: "string";
-      UserPoolId: "string";
-      UserPoolArn: "string";
-      UserPoolName: "string";
-      AppClientIDWeb: "string";
-      AppClientID: "string";
-      CreatedSNSRole: "string";
-    };
-  };
-  function: {
-    fsCognitoPostConfirmationInvoker: {
-      Name: "string";
-      Arn: "string";
-      Region: "string";
-      LambdaExecutionRole: "string";
-    };
-    fsDailyReport: {
-      Name: "string";
-      Arn: "string";
-      Region: "string";
-      LambdaExecutionRole: "string";
-      CloudWatchEventRule: "string";
-    };
-    fsCoreApiResolver: {
-      Name: "string";
-      Arn: "string";
-      Region: "string";
-      LambdaExecutionRole: "string";
-    };
-    fsCognitoPostConfirmation: {
-      Name: "string";
-      Arn: "string";
-      Region: "string";
-      LambdaExecutionRole: "string";
-    };
-  };
-};
+    "api": {
+        "fsCoreAPI": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "userPoolGroups": {
+            "freeGroupRole": "string",
+            "proGroupRole": "string"
+        },
+        "fsCognito": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "function": {
+        "fsDailyReport": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string",
+            "CloudWatchEventRule": "string"
+        },
+        "fsCoreApiResolver": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "fsCognitoPostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    }
+}
