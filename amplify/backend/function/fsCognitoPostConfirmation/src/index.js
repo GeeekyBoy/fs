aa480822c10b5807f3c00a707e33037c182dfc79
Ahmed Hassan <ahmed__hassan@outlook.com>
2022-08-28 12:05:20 +0200

    bug fix

--- amplify/backend/function/fsCognitoPostConfirmation/src/index.js
+++ amplify/backend/function/fsCognitoPostConfirmation/src/index.js
@@ -1,33 +1,65 @@
-/**
- * @fileoverview
- *
- * This CloudFormation Trigger creates a handler which awaits the other handlers
- * specified in the `MODULES` env var, located at `./${MODULE}`.
- */
+/* Amplify Params - DO NOT EDIT
+  AUTH_FSCOGNITO_USERPOOLID
+  ENV
+  REGION
+Amplify Params - DO NOT EDIT */
 
-/**
- * The names of modules to load are stored as a comma-delimited string in the
- * `MODULES` env var.
- */
-const moduleNames = process.env.MODULES.split(',');
-/**
- * The array of imported modules.
- */
-const modules = moduleNames.map(name => require(`./${name}`));
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+require('dotenv').config();
 
-/**
- * This async handler iterates over the given modules and awaits them.
- *
- * @see https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html#nodejs-handler-async
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- * 
- */
-exports.handler = async (event, context) => {
-  /**
-   * Instead of naively iterating over all handlers, run them concurrently with
-   * `await Promise.all(...)`. This would otherwise just be determined by the
-   * order of names in the `MODULES` var.
-   */
-  await Promise.all(modules.map(module => module.handler(event, context)));
-  return event;
+const {
+  SENDGRID_API_KEY,
+  RDS_HOST,
+  RDS_USER,
+  RDS_PASSWORD,
+} = process.env;
+
+const aws = require('aws-sdk');
+const sgMail = require('@sendgrid/mail');
+const mariadb = require('mariadb');
+const getEmailContent = require('./email/index').getContent;
+
+const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({
+  apiVersion: '2016-04-18',
+});
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const pool = mariadb.createPool({
+  host: RDS_HOST,
+  user: RDS_USER,
+  password: RDS_PASSWORD,
+  database: 'forwardslash',
+  connectionLimit: 5,
+});
+
+exports.handler = async (event) => {
+  try {
+    const query = 'CALL create_user(?, ?, ?, ?)';
+    const params = [
+      event.userName,
+      event.request.userAttributes.given_name,
+      event.request.userAttributes.family_name,
+      event.request.userAttributes.email,
+    ];
+    const addUserParams = {
+      GroupName: 'free',
+      UserPoolId: event.userPoolId,
+      Username: event.userName,
+    };
+    await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
+    await pool.execute(query, params);
+    const emailToBeSent = getEmailContent('accountCreation', {
+      FIRST_NAME: params[1],
+    });
+    await sgMail.send({
+      to: params[3],
+      from: 'notify@forwardslash.ch',
+      subject: emailToBeSent.subject,
+      html: emailToBeSent.body,
+    });
+    return event;
+  } catch (err) {
+    throw new Error(err);
+  }
 };
