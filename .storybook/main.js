52144d389d49622f5581d968b23b1cfba18d8bf2
Ahmed Hassan <ahmed__hassan@outlook.com>
2022-11-27 02:05:16 +0200

    Upgrade to storybook 7.0

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,14 @@
-const { mergeConfig } = require('vite');
-const svgr = require('vite-plugin-svgr');
-
-module.exports = {
+export default {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../src/public"],
   core: {
     builder: "@storybook/builder-vite",
   },
+  framework: "@storybook/react-vite",
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      define: {
-        global: "window",
-      },
-      plugins: [svgr({ svgrOptions: { ref: true } })],
-    });
-  },
 };
